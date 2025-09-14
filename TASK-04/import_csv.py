64a5b0dacb1a1e252a729f4bee1e6d901bc4e5d7
Yaksh Chauhan <yakshchauhan2@gmail.com>
2025-09-14 23:26:33 +0530

    Update import_csv.py

--- TASK-04/import_csv.py
+++ TASK-04/import_csv.py
@@ -1,1 +1,16 @@
+import mysql.connector
+
+def get_connection():
+    try:
+        conn = mysql.connector.connect(
+            host="127.0.0.1",   # your local MySQL server
+            user="yaksh",       # your MySQL username
+            password="yaksh",   # your MySQL password
+            database="moviesdb" # the database you imported movieslist.sql into
+        )
+        return conn
+    except mysql.database.error as error:
+        print(f"error in database connection : {error}")
+        return None
+
 
